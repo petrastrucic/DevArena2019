ecbbd5095e2a676477bb24e3ee9412fe09137123
petrar2d2 <petrar2d2@gmail.com>
2019-09-29 13:31:50 +0200

    .git ignore added

--- DevArena2019.SendEmail/SendEmail.cs
+++ DevArena2019.SendEmail/SendEmail.cs
@@ -1,5 +1,8 @@
 using Microsoft.Azure.ServiceBus;
 using Microsoft.Azure.WebJobs;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Collections.Generic;
 
 namespace DevArena2019.SendSms
 {
@@ -10,7 +13,22 @@ namespace DevArena2019.SendSms
             [ServiceBusTrigger("%ServiceBusTranslationQueue%", 
             Connection = "ServiceBusConnectionString")]Message mySbMsg)
         {
+            var msg = new SendGridMessage();
 
+            string str = System.Text.Encoding.Default.GetString(mySbMsg.Body);
+
+            msg.SetFrom(new EmailAddress("dx@example.com", "SendGrid DX Team"));
+
+            var recipients = new List<EmailAddress>
+                {
+                    new EmailAddress("pstrucic@ekobithr.onmicrosoft.com", "Petra Strucic")
+                };
+            msg.AddTos(recipients);
+
+            msg.SetSubject("Testing the SendGrid C# Library");
+
+            msg.AddContent(MimeType.Text, "Hello World plain text!");
+            msg.AddContent(MimeType.Html, "<p>Hello World!</p>");
         }
     }
 }
